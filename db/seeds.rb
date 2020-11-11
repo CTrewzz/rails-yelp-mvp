678068073ffff34659e875995cfe8b96c76dce29
Vasil Hodzhev <vasilhodzhev@gmail.com>
2020-11-11 13:13:04 +0100

    Yelp MVP working, style missing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+8.times do
+  Restaurant.create(name: Faker::Restaurant.name,
+                    address: Faker::Address.street_address,
+                    phone_number: Faker::PhoneNumber.cell_phone,
+                    category: ["chinese", "italian", "japanese", "french", "belgian"].sample)
+end
